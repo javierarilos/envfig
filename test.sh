3c92edce4892a1b9b51b4f4f67fc3879450eb794
javier.arilos <javier.arilos@gmail.com>
2014-12-09 13:13:35 +0100

    publish coverage results to codeclimate.com

--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@
 set -e
 set -x
 export PATH=$PATH:./node_modules/.bin
-istanbul cover mocha -- -R list -u tdd test/
+istanbul cover ./node_modules/.bin/_mocha -- -u tdd test/
+
+CODECLIMATE_REPO_TOKEN=5ed3f7099f54387ec83146bf3effe83e2b0a27b0d5f9acee4f9dea7f0ff27e58 codeclimate < coverage/lcov.info
